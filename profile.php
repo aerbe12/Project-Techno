78f5e9058cffb0ce5a6b8d68a4bfc6f13871024d
Rivaldo <rivaldorizky77@gmail.com>
2023-03-27 16:35:16 +0800

    update

--- profile.php
+++ profile.php
@@ -22,20 +22,57 @@ include './Components/Login/loginsession.php';
 
     <!-- PROFILE -->
     <div class="profile-container">
+      <!-- PROFILE IMAGE -->
       <div class="profile-image">
-        <img src="./img/profile.jpg" alt="Your Profile Image" />
-      </div>
-      <div class="profile-data-container">
-        <div class="profile-data">
-          <h2>Data diri pengguna</h2>
-          <?php include './Components/Login/profiledata.php'?>
+      <?php
+     // Check if the user has a saved image path in a cookie
+          if (isset($_COOKIE['image_'.$_SESSION['id_user']])) {
+            $image = "uploads/" . $_COOKIE['image_'.$_SESSION['id_user']];
+          } else {
+            // Set default image path
+            $image = "./img/avatar.svg";
+          }
+
+          if (isset($_POST['submit'])) {
+            // Check if a file was uploaded successfully
+            if ($_FILES['image']['error'] === UPLOAD_ERR_OK) {
+              $tmp_name = $_FILES['image']['tmp_name'];
+              $name = $_SESSION['id_user'] . ".jpg"; // use the user's ID as the filename
+              $image = "./uploads/" . $name;
+              // Move uploaded file to destination directory
+              move_uploaded_file($tmp_name, $image);
+              // Save the new image path in a cookie
+              setcookie('image_'.$_SESSION['id_user'], $name, time() + (86400 * 30), "/"); // cookie expires in 30 days
+            }
+          }
+
+          // Update $image with the saved image path from the cookie
+          if (isset($_COOKIE['image_'.$_SESSION['id_user']])) {
+            $image = "uploads/" . $_COOKIE['image_'.$_SESSION['id_user']];
+          }
+      
+        ?>
+    <img src="<?php echo $image ?>" alt="Your Profile Image" />
+    <form class="photo-profile"action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
+      <input  type="file" name="image" accept=".jpg,.jpeg,.png" />
+      <button class="btn-submit" type="submit" name="submit">Upload</button>
+    </form>
+        </div>
+        <!-- PROFILE IMAGE END -->
+
+        <div class="profile-data-container">
+          <div class="profile-data">
+            <h2>Data diri pengguna</h2>
+            <?php include './Components/Login/profiledata.php'?>
+          </div>
         </div>
       </div>
-    </div>
-    <!-- PROFILE END -->
-
-    <!-- NO Transaction -->
- <?php include "./Components/Transaction/noTransaction.php" ?>
-    <!-- NO Transaction END-->
-  </body>
-</html>
+      <!-- PROFILE END -->
+      
+      <!-- NO Transaction -->
+      <?php include "./Components/Transaction/noTransaction.php" ?>
+      <!-- NO Transaction END-->
+    </body>
+    </html>
+    
+    <!-- <img src="./img/profile.jpg" alt="Your Profile Image" /> -->
